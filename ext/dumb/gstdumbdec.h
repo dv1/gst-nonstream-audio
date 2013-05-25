92e9819a85ac2f8515f578e0b5fcf73c95ac1ad4
Carlos Rafael Giani <dv@pseudoterminal.org>
2013-05-25 16:58:15 +0200

    renamed dumb to dumbdec & begun GME decoder
    Signed-off-by: Carlos Rafael Giani <dv@pseudoterminal.org>

---
+++ ext/dumb/gstdumbdec.h
@@ -0,0 +1,46 @@
+#ifndef GSTDUMBDEC_H
+#define GSTDUMBDEC_H
+
+#include <gst/gst.h>
+#include <dumb.h>
+#include "gstnonstreamaudiodecoder.h"
+
+
+G_BEGIN_DECLS
+
+
+typedef struct _GstDumbDec GstDumbDec;
+typedef struct _GstDumbDecClass GstDumbDecClass;
+
+
+#define GST_TYPE_DUMB_DEC             (gst_dumb_dec_get_type())
+#define GST_DUMB_DEC(obj)             (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_DUMB_DEC, GstDumbDec))
+#define GST_DUMB_DEC_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_DUMB_DEC, GstDumbDecClass))
+#define GST_IS_DUMB_DEC(obj)          (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_DUMB_DEC))
+#define GST_IS_DUMB_DEC_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_DUMB_DEC))
+
+
+struct _GstDumbDec
+{
+	GstNonstreamAudioDecoder parent;
+
+	gint sample_rate, num_channels;
+	DUH *duh;
+	DUH_SIGRENDERER *duh_sigrenderer;
+};
+
+
+struct _GstDumbDecClass
+{
+	GstNonstreamAudioDecoderClass parent_class;
+};
+
+
+GType gst_dumb_dec_get_type(void);
+
+
+G_END_DECLS
+
+
+#endif
+
